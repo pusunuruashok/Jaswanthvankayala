1025f15be6af6eb25bd9f44b535b853d852fd01b
Ashok Pusunuru <pusunuruashok99@gmail.com>
2026-04-20 20:04:46 -0400

    the hideing details in the system has been foudn

---
+++ One.java
@@ -0,0 +1,8 @@
+public class One {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming in  the system propelry to make appear in the system.");
+    }
+}
